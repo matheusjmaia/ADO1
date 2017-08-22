f993589f42aecd48105d4490064d9c49585ede12
matheus.jmaia <matheus.jmaia@CAS511886W0726.senacsp.edu.br>
2017-08-21 21:37:49 -0300

    até o EXE5

---
+++ src/ado1/Exercicio5.java
@@ -0,0 +1,23 @@
+
+package ado1;
+
+import static java.lang.Math.random;
+import java.util.Random;
+
+/**
+ *
+ * @author matheus.jmaia
+ */
+public class Exercicio5 {
+    //função sorteia um número aleatorio e retorna o mn
+    static int sorteiaNumero(){
+    Random rng = new Random ();
+    int numeroAleatorio = rng.nextInt(99);
+    return numeroAleatorio;
+}
+    
+    
+    public static void main(String[] args) {
+     
+    }
+}
